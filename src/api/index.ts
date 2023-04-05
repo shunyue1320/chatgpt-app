23ad62c845918155cfca7afc1de52fac8f5e9c96
shunyue1320 <tsunyue1320@gmail.com>
2023-04-05 21:16:00 +0800

    chore: 添加 `/chat-process` 请求接口

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import type { AxiosProgressEvent, GenericAbortSignal } from 'axios'
+import { post } from '@/utils/request'
+import { useSettingStore } from '@/store'
+
+export function fetchChatAPIProcess<T = any>(
+  params: {
+    prompt: string
+    options?: { conversationId?: string; parentMessageId?: string }
+    signal?: GenericAbortSignal
+    onDownloadProgress?: (progressEvent: AxiosProgressEvent) => void
+  },
+) {
+  const settingStore = useSettingStore()
+
+  return post<T>({
+    url: '/chat-process',
+    data: { prompt: params.prompt, options: params.options, systemMessage: settingStore.systemMessage },
+    signal: params.signal,
+    onDownloadProgress: params.onDownloadProgress,
+  })
+}

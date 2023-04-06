1a0559a0e33c484928c95cbc32fc0a62d3824480
shunyue1320 <tsunyue1320@gmail.com>
2023-04-06 21:56:55 +0800

    chore: 样式优化

--- src/views/chat/components/Message/index.vue
+++ src/views/chat/components/Message/index.vue
@@ -16,15 +16,21 @@ const asRawText = ref(props.inversion)
 </script>
 
 <template>
-  <div class="flex w-full mb-6 overflow-hidden" :class="[{ 'flex-row-reverse': inversion }]">
-    <div class="flex items-center justify-center flex-shrink-0">
+  <div class="flex w-full mb-6 overflow-hidden items-start" :class="[{ 'flex-row-reverse': inversion }]">
+    <div
+      class="flex items-center justify-center flex-shrink-0"
+      :class="[inversion ? 'ml-2' : 'mr-2']"
+    >
       <AvatarComponent :image="inversion" />
     </div>
     <div class="overflow-hidden text-sm" :class="[inversion ? 'items-end' : 'items-start']">
       <p class="text-xs text-gray-400" :class="[inversion ? 'text-right' : 'text-left']">
         {{ dateTime }}
       </p>
-      <div class="flex items-end gap-1 mt-2">
+      <div
+        class="flex items-end gap-1 mt-2"
+        :class="[inversion ? 'flex-row-reverse' : 'flex-row']"
+      >
         <TextComponent
           :inversion="inversion"
           :error="error"

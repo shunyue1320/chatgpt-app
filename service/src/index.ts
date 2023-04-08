990bf700ff05300752adc4ddd65bc9ca14b290e0
shunyue1320 <tsunyue1320@gmail.com>
2023-04-08 18:11:56 +0800

    chore: 添加环境变量

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,3 +1,4 @@
+import './preload'
 import Koa from 'koa'
 import serve from 'koa-static'
 import bodyparser from 'koa-bodyparser'
@@ -19,6 +20,6 @@ app.use(cors({ origin: '*' }))
 
 app.use(chatgptRouter.routes())
 
-app.listen(3011, () => {
-  globalThis.console.log('Server started on port 3011')
+app.listen(3010, () => {
+  globalThis.console.log('Server started on port 3010')
 })
